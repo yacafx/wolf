b653e82cc553101464ecd26db473990a5a3a9252
yacaFx <sergiobritor@gmail.com>
2017-10-10 14:07:42 -0500

    Se modifico la forma de acceso al puerto

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // console.log('Hola mundo!', 'Fuck yeah!');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
-})
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
